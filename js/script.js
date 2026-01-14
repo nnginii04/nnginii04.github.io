de8ec71e452568e0df8cf2152abf427ccba9a816
yejin <nnginii040416@gmail.com>
2026-01-14 11:12:53 +0900

    fix : contact 섹션 active 보정 및 하단 여백 추가

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function () {
-    var NAV_OFFSET = 70;
+    var NAV_OFFSET = 80;
+    var $body = $("body");
+    var $navLinks = $(".navbar-nav li a");
+    var $collapse = $(".navbar-collapse");
 
-    $(".navbar a, footer a[href='#myPage'], a[href='#background']").on("click", function (event) {
+    $body.scrollspy({ target: ".navbar", offset: NAV_OFFSET });
+    $(window).on("load resize", function () {
+        $body.scrollspy("refresh");
+        syncActiveNav(); // 리사이즈 시에도 바로 맞춤
+    });
+
+    // Smooth scroll
+    $navLinks.on("click", function (event) {
         var hash = this.hash;
         if (!hash) return;
 
@@ -10,14 +20,66 @@ $(document).ready(function () {
 
         event.preventDefault();
 
-        $("html, body").animate(
-            { scrollTop: $target.offset().top - NAV_OFFSET },
-            650,
-            function () { window.location.hash = hash; }
+        $("html, body").stop().animate(
+            { scrollTop: $target.offset().top - (NAV_OFFSET - 1) },
+            520,
+            "swing",
+            function () {
+                window.location.hash = hash;
+                syncActiveNav(); // 이동 끝나고 바로 active 동기화
+            }
         );
 
         if ($(".navbar-toggle").is(":visible")) {
-            $(".navbar-collapse").collapse("hide");
+            $collapse.collapse("hide");
         }
     });
+
+    // Sections list
+    var sectionIds = $navLinks.map(function () {
+        return $(this).attr("href");
+    }).get();
+
+    function syncActiveNav() {
+        var scrollTop = $(window).scrollTop();
+        var scrollPos = scrollTop + NAV_OFFSET + 2;
+
+        // ✅ 맨 아래 근처면 무조건 CONTACT
+        var docH = $(document).height();
+        var winH = $(window).height();
+        if (scrollTop + winH >= docH - 2) {
+            setActive("#contact");
+            return;
+        }
+
+        var current = sectionIds[0] || "#about";
+        for (var i = 0; i < sectionIds.length; i++) {
+            var id = sectionIds[i];
+            var $sec = $(id);
+            if ($sec.length && $sec.offset().top <= scrollPos) {
+                current = id;
+            }
+        }
+        setActive(current);
+    }
+
+    function setActive(id) {
+        $(".navbar-nav li").removeClass("active");
+        $(".navbar-nav li a[href='" + id + "']").parent().addClass("active");
+    }
+
+    // rAF scroll
+    var ticking = false;
+    $(window).on("scroll", function () {
+        if (!ticking) {
+            window.requestAnimationFrame(function () {
+                syncActiveNav();
+                ticking = false;
+            });
+            ticking = true;
+        }
+    });
+
+    // init
+    syncActiveNav();
 });
